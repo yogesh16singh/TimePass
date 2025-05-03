7b08ef3201c924ff0fec587cf0b213b596c733d8
yogesh16singh <ys1997642@gmail.com>
2025-05-03 17:15:17 +0530

    build update

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -14,7 +14,7 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/game-crud";
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://time-pass-two.vercel.app/'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://time-pass-two.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use(express_1.default.json());
